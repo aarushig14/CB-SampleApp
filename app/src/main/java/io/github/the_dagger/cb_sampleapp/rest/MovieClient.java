df8d0763663d6d6dec6ff254fc04495917fed5a3
Harshit Dwivedi <harshithdwivedi@gmail.com>
2017-02-04 11:12:30 +0530

    Update MovieClient.java

--- app/src/main/java/io/github/the_dagger/cb_sampleapp/rest/MovieClient.java
+++ app/src/main/java/io/github/the_dagger/cb_sampleapp/rest/MovieClient.java
@@ -13,6 +13,4 @@ public interface MovieClient {
     @GET("movie/popular?api_key=97f469b9e89b30f1f7d07e8b35973e56&page=1&language=en-US")
     Call<MovieList> getMovieList();
 
-//    @GET("search/users?q=harshit")
-//    Call<UserList> getUsers();
 }
